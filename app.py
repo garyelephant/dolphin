f88a08be872f1c3e44c5ba0e918161d0af58e1ad
garyelephant <garygaowork@gmail.com>
2015-07-27 13:29:26 +0000

    aysnchrouous to retrive parsed log

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,request
 from flask import render_template
 
 app = Flask(__name__)
@@ -11,10 +11,19 @@ def hello():
 def index():
     return render_template( 'index.html' )
 
+@app.route( "/parse_log", methods=[ 'POST' ] )
+def parse_log():
+    import json
+    form = request.get_data()
+
+    form = json.loads( form )
+
+    # TODO
+    return '{ "km": "vm" }'
+
 @app.route("/sortable_example")
 def sortable_example():
     return render_template( 'sortable_example.html' )
-
 
 ep = 'select'
 @app.route("/{endpoint}".format(endpoint=ep))
